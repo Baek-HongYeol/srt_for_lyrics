32e427a1c02837d301ceac7104c19f201ecfb6ca
Baek-HongYeol <bac25450@gmail.com>
2023-01-16 07:05:40 +0900

    config 파일 json으로 변경. sample config 추가

--- config.py
+++ config.py
@@ -1,33 +1,44 @@
 import os
+import json
 
 class MyConfig:
     def __init__(self):
+        self.load_config()
         self.audio_filename = None
         self.sample_rate = None
         self.lyric_filename = None
         self.isDefault = True
+
         self.find_audio_file()
         pass
 
-    def load_dev(self, i):
-        txt = ''
-        if os.path.exists("test.txt"):
-            with open("test.txt", "r", encoding="UTF-8") as f:
-                txt = f.read().splitlines()
-            return txt[i]
+    def load_config(self):
+        """
+        load configs from config.json in same directory.
+        """
+        self.config = {}
+        if os.path.exists("config.json"):
+            with open("config.json", "r", encoding="UTF-8") as f:
+                self.config = json.loads(f.read())
+    
+    def get_setting(self, key):
+        if key in self.config:
+            return self.config[key]
+        else:
+            return None
 
 
     def find_audio_file(self, change:bool=False):
         self.isDefault = not change
         sample_rate = -1
-        audio_filename = (self.load_dev(0) if self.isDefault else None) or input("오디오 파일 이름 혹은 경로를 입력하세요. :\n")
+        audio_filename = (self.get_setting('audio_filename') if self.isDefault else None) or input("오디오 파일 이름 혹은 경로를 입력하세요. :\n")
         while not os.path.exists(audio_filename):
             audio_filename = input("존재하지 않는 파일 혹은 경로입니다. \n오디오 파일 이름 혹은 경로를 입력하세요. :\n")
         self.audio_filename:str = audio_filename
         while True:
             #TODO 변경 취소 만들기
             try:
-                sample_rate = self.load_dev(1) or int(input("오디오 sample rate를 입력하세요.(44100, 48000): "))
+                sample_rate = self.get_setting('sample_rate') or int(input("오디오 sample rate를 입력하세요.(44100, 48000): "))
                 sample_rate = int(sample_rate)
                 if sample_rate > 0:
                     break
@@ -39,7 +50,7 @@ class MyConfig:
     
     def set_lyric_file(self):
         # TODO 취소 만들기
-        lyric_filename = self.load_dev(2) or input("등록할 스크립트 파일 이름 혹은 경로를 입력하세요. :\n")
+        lyric_filename = self.get_setting('lyric_filename') or input("등록할 스크립트 파일 이름 혹은 경로를 입력하세요. :\n")
         while not os.path.exists(lyric_filename):
             lyric_filename = input("존재하지 않는 파일 혹은 경로입니다. 다시 입력하세요.\n")
         self.lyric_filename = lyric_filename
